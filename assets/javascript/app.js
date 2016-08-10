e260448c5823f3cc6c49fa6cf7c46406b1afd636
Kevin Shukla <kevn1841@gmail.com>
2016-08-10 18:25:54 -0400

    Added js ADDBUTTON functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,29 @@
 var Sports = ["Basketball", "Football", "Soccer", "Baseball", "Tennis", "Golf"];
+
+var a;
+
 function loadSportButtons(){
      for (i=0;i<Sports.length;i++){
-            var a = $('<button>')
+            a = $('<button>')
             a.text(Sports[i]);
             $(listedSports).append(a);
         }
 }
+
 loadSportButtons()
+
+$('#submit').on('click', function(){
+    var newSport = document.getElementById('userInput').value;
+    Sports = [];
+    Sports.push(newSport);
+    loadSportButtons();
+    console.log(Sports);
+});
+
+$("a").click(function(){
+    console.log(a[i]);
+})
+
+$('button').on('click', function(){
+    console.log(a);
+});
